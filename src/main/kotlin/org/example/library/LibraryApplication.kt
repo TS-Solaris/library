37e9eca19c5985ab23909a82a4edb432d4fe77b5
Tetsu Suzuki <52594376+TS-Solaris@users.noreply.github.com>
2025-06-20 18:03:14 +0900

    Revert "Update LibraryApplication.kt"
    This reverts commit d8c3ce9c10452717e32bfd3621f5e7eb69353e33.

--- src/main/kotlin/org/example/library/LibraryApplication.kt
+++ src/main/kotlin/org/example/library/LibraryApplication.kt
@@ -2,16 +2,10 @@ package org.example.library
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import javax.crypto.Cipher
 
 @SpringBootApplication
 class LibraryApplication
 
 fun main(args: Array<String>) {
-
-    Cipher.getInstance("AES/CBC/PKCS5Padding")
-
     runApplication<LibraryApplication>(*args)
 }
-
-private const val ECS_METADATA_ENDPOINT = "http://192.168.0.1"
